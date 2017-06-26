2b399d1067e3a8d83011db287bd4ccdf5de783ec
Arathy <arathyjanardhanan@gmail.com>
2017-06-26 19:53:48 +1000

    Reafctoring Payslip to make it more readable

--- src/main/java/com/myob/employeemontlypayslip/models/Payslip.java
+++ src/main/java/com/myob/employeemontlypayslip/models/Payslip.java
@@ -1,26 +1,39 @@
 package com.myob.employeemontlypayslip.models;
 
 public class Payslip {
+    private static final int NO_OF_MONTHS_IN_YEAR = 12;
     private String name;
     private PaymentPeriod paymentPeriod;
     private Long grossIncome;
     private Long incomeTax;
     private Long netIncome;
-    private Long superAnnuation;
+    private Long superannuation;
 
     public static Payslip createInstance(Employee employee) {
         Payslip payslip = new Payslip();
-        payslip.name = String.format("%s %s", employee.getFirstName(), employee.getLastName());
+        payslip.name = getFullName(employee);
         payslip.paymentPeriod = employee.getPaymentPeriod();
-        payslip.grossIncome = Math.round(employee.getAnnualSalary()/12);
-        payslip.incomeTax = Math.round(IncomeTax.calculateIncomeTax(employee.getAnnualSalary())/12);
-        payslip.netIncome = payslip.getNetIncome();
-        payslip.superAnnuation = Math.round(payslip.grossIncome * employee.getSuperRate() / 100);
+        payslip.grossIncome = getGrossIncome(employee);
+        payslip.incomeTax = getIncomeTax(employee);
+        payslip.netIncome = payslip.grossIncome - payslip.incomeTax;
+        payslip.superannuation = getSuperannuation(employee, payslip);
         return payslip;
     }
 
-    private long getNetIncome() {
-        return grossIncome - incomeTax;
+    private static long getSuperannuation(Employee employee, Payslip payslip) {
+        return Math.round(payslip.grossIncome * employee.getSuperRate() / 100);
+    }
+
+    private static long getIncomeTax(Employee employee) {
+        return Math.round(IncomeTax.calculateIncomeTax(employee.getAnnualSalary()) / NO_OF_MONTHS_IN_YEAR);
+    }
+
+    private static String getFullName(Employee employee) {
+        return String.format("%s %s", employee.getFirstName(), employee.getLastName());
+    }
+
+    private static long getGrossIncome(Employee employee) {
+        return Math.round(employee.getAnnualSalary()/ NO_OF_MONTHS_IN_YEAR);
     }
 
     @Override
@@ -30,6 +43,6 @@ public class Payslip {
                 + grossIncome + ","
                 + incomeTax + ","
                 + netIncome + ","
-                + superAnnuation;
+                + superannuation;
     }
 }
